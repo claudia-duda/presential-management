45fcdadf51624a21b512b728a6a92465ea897d51
Claudia <claudiaeduarda_duda@hotmail.com>
2021-11-18 12:26:14 -0300

    Initial Commit

---
+++ src/main/java/com/managment/presentialmanagment/PresentialmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.managment.presentialmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentialmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentialmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managment/presentialmanagment/PresentialmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managment.presentialmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentialmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
